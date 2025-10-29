b6c467f1b0d7e08153ab1d624142584d5fba88de
Abhishek Jain <jainabhi1607@gmail.com>
2025-10-29 17:23:15 +0530

    adding files

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { connectToDB } from "./config/db.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import productRouter from "./routes/productRoutes.js";
-import cartRouter from "./routes/cartRoutes.js";	
+import cartRouter from "./routes/cartRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import wishlistRouter from "./routes/wishlistRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-import { rateLimit } from 'express-rate-limit';
+import { rateLimit } from "express-rate-limit";
 
 // Load environment variables from .env file
 import dotenv from "dotenv";
@@ -19,26 +19,26 @@ app.use(express.json());
 await connectToDB();
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 };
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/product", productRouter);
 app.use("/cart", cartRouter);
 app.use("/auth", authRouter);
-app.use('/wishlist',wishlistRouter);
-app.use('/order',orderRouter);
+app.use("/wishlist", wishlistRouter);
+app.use("/order", orderRouter);
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    // store: ... , // Redis, Memcached, etc. See below.
+});
 
 app.use(limiter);
 
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,11 @@
 import {Router} from 'express';
-import {createOrder,fetchOrders,singleOrder} from '../controllers/orderController.js';
+import {createOrder,fetchOrders,singleOrder,updateOrderStatus} from '../controllers/orderController.js';
 
 const orderRouter = Router();
 
 orderRouter.post('/create',createOrder);
 orderRouter.get('/fetch',fetchOrders);
 orderRouter.get('/single/:id',singleOrder);
-//orderRouter.put('/update-status/:id',updateOrderStatus);
+orderRouter.put('/update-status/:id',updateOrderStatus);
 
 export default orderRouter;

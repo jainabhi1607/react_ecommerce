6986b6c1a57374f76dc905b89cc2191f913a23fb
Abhishek Jain <jainabhi1607@gmail.com>
2025-11-27 09:11:14 +0530

    Small change

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -124,8 +124,10 @@ import { Router } from "express";
 import multer from "multer";
 import { fetchProduct, fetchSingleProduct, addProduct, editProduct, deleteProduct } from "../controllers/productController.js";
 import path from 'path';
+import express from "express";
 
-const productRouter = Router();
+
+const productRouter = express.Router();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) { cb(null, './uploads') },
   filename: function (req, file, cb) {
@@ -136,10 +138,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-productRouter.get("/get", fetchProduct);
-productRouter.post("/add", upload.single('image'), addProduct);
-productRouter.put("/edit/:id", upload.single('image'), editProduct);
 productRouter.get("/product/:id", fetchSingleProduct);
+productRouter.get("/get", fetchProduct);
+productRouter.get("/get/:pageNumber", fetchProduct);
+productRouter.post("/add", upload.single("image"), addProduct);
+productRouter.put("/edit/:id", upload.single("image"), editProduct);
 productRouter.delete("/delete/:id", deleteProduct);
 
 export default productRouter;
